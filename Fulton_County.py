bf50f421d961d95d0f01f45a94bf0c06c4ab2868
Kyrillos_Akram <kerillos.akram@gmail.com>
2019-10-17 00:53:54 +0200

    adding a data structure

--- Fulton_County.py
+++ Fulton_County.py
@@ -1,7 +1,22 @@
 from bs4 import BeautifulSoup
 from urllib import urlopen
 import csv
+from multiprocessing import cpu_count,Pool
 
+inputs_path		="inputs.txt"
+propertyHeader	=['Input','ParcelID','Location Address','PropertyType','Neighborhood','Acreage','Exemption Codes','CurrentOwner','OwnerAddress','city','state','zipcode','structure','Units','Year Built','Sq ft','stories','construction','style','Year built','Sq Ft','basement','Full Bath','Half Bath','bedrooms','value']
+salesHeader		=['SalesDate','SalesPrice','SalesType','Buyer','Seller']
+sales_writer	=csv.writer(open("sales.csv",'w'))
+property_writer	=csv.writer(open("property.csv",'w'))
+sales_writer.writerow(salesHeader)
+property_writer.writerow(propertyHeader)
+inputs			=[line.strip() for line in open(inputs_path,'r')]
+number_of_inputs=len(inputs)
+current			=0
+number_of_cpus	=cpu_count()
+print("found %i cpu and will be used to accelerate the proccess")
+
+result_database=[]
 
 def decodekeyvalue(inputdata):
 	keyvalue =inputData		
@@ -23,13 +38,15 @@ def decodekeyvalue(inputdata):
 		keyvalue=keyvalue[0:2]+'+'+keyvalue[2:]
 	return keyvalue
 
-keyvalue =decodekeyvalue(inputData)
-print(keyvalue)
+#keyvalue =decodekeyvalue(inputData)
+#print(keyvalue)
 
 def extract(key):
 	input_key		=key
 	key				=decodekeyvalue(key)
 	url				="https://qpublic.schneidercorp.com/Application.aspx?AppID=936&LayerID=18251&PageTypeID=4&PageID=8156&Q=1271885427&KeyValue="+key
+	print("[%0.3i]\tscraping %s"%(current,input_key))
+	current			=current+1
 	response		=urlopen(url)
 	page			=BeautifulSoup(response,'lxml')
 	Parcel_Number   =page.find("td",text="Parcel Number").nextSibling()
@@ -73,14 +90,31 @@ def extract(key):
 		Half_Bath	=Full_Half[1]
 		bedrooms	=page.find('section',{"id":"ctlBodyPane_ctl03_mSection"}).find("td",text="bedrooms").nextSibling().get_text()
 		Total_Value	=page.find('section',{"id":"ctlBodyPane_ctl03_mSection"}).find("td",text="Total Value").nextSibling().get_text()
-	
-	propertyHeader	=['ParcelID','Location Address','PropertyType','Neighborhood','Acreage','Exemption Codes','CurrentOwner','OwnerAddress','city','state','zipcode','structure','Units','Year Built','Sq ft','stories','construction','style','Year built','Sq Ft','basement','Full Bath','Half Bath','bedrooms','value']
-	salesHeader		=['SalesDate','SalesPrice','SalesType','Buyer','Seller']
+	else:
+		stories		=''
+		construction=''
+		style		=''
+		year_built_r=''
+		Res_Sq_Ft	=''
+		basement	=''
+		Full_Half	=''
+		Full_Bath	=''
+		Half_Bath	=''
+		bedrooms	=''
+		Total_Value =''
 
 	SaleDate		=page.xpath('/html/body/form/div[5]/div/div[1]/main/section[5]/div/div/table/tbody').findNext('td').get_text()
 	SalesPrice		=page.xpath('/html/body/form/div[5]/div/div[1]/main/section[5]/div/div/table/tbody').findNext('td').findNext('td').get_text()
 	salesValidity	=page.xpath('/html/body/form/div[5]/div/div[1]/main/section[5]/div/div/table/tbody/tr/td[7]').get_text()
 	Buyer			=page.xpath('/html/body/form/div[5]/div/div[1]/main/section[5]/div/div/table/tbody/tr/td[8]').get_text()
 	Seller			=page.xpath('/html/body/form/div[5]/div/div[1]/main/section[5]/div/div/table/tbody/tr/td[9]').get_text()
+
+	return [[SaleDate,SalesPrice,salesValidity,Buyer,Seller],[input_key,Parcel_Number,Location_Address,Property_Class,Neighborhood,Acres,Exceptions,CurrentOwner,OwnerAddress,streetAddress,city,state,zipcode,structure,units,year_built_c,total_sq_footage,stories,construction,style,year_built_r,Res_Sq_Ft,basement,Full_Bath,Half_Bath,bedrooms,Total_Value]] 
+
+with Pool(processes=number_of_cpus) as pool:
 	
-	return
+
+sales_writer.writerow([SaleDate,SalesPrice,salesValidity,Buyer,Seller])
+property_writer.writerow([input_key,Parcel_Number,Location_Address,Property_Class,Neighborhood,Acres,Exceptions,CurrentOwner,OwnerAddress,streetAddress,city,state,zipcode,structure,units,year_built_c,total_sq_footage,stories,construction,style,year_built_r,Res_Sq_Ft,basement,Full_Bath,Half_Bath,bedrooms,Total_Value])
+	
+	
